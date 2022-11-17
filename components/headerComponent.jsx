f4d954c5b7165b15d8a9e49f34ca43820ea930f4
gabriel valera <hashiramakage@gmail.com>
2022-11-16 21:42:44 -0400

    agregando menu movil completo

--- components/headerComponent.jsx
+++ components/headerComponent.jsx
@@ -4,7 +4,9 @@ export default function HeaderComponent(){
 
     function mostarMenu(){
         let $contenedorMenu = document.getElementById("contenedorMenu")
+        let $menuMovil = document.getElementById("menuMovil")
         $contenedorMenu.classList.toggle("ocultar")
+        $menuMovil.classList.toggle("mostra-menu-movil")
     }
 
 
@@ -41,7 +43,7 @@ export default function HeaderComponent(){
                                         </li>
                                         <li>
                                             <Link className="link-menu-movil" href="#">
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" stroke-linejoin="round" className="feather feather-mail"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path><polyline points="22,6 12,13 2,6"></polyline></svg>
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-mail"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path><polyline points="22,6 12,13 2,6"></polyline></svg>
                                                 Contacto
                                             </Link>
                                         </li>
