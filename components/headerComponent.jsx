69d432a0b35d2162e6516a9b354587eace961049
gabriel valera <hashiramakage@gmail.com>
2022-11-29 14:24:09 -0400

    ahora el menu desktop va estar siempre visible

--- components/headerComponent.jsx
+++ components/headerComponent.jsx
@@ -3,23 +3,17 @@ import Link from "next/link"
 export default function HeaderComponent(){
 
     function mostarMenu(){
-        let $menuDesktop = document.getElementById("menuDesktop")
+        // let $menuDesktop = document.getElementById("menuDesktop")
         let $menuMovil = document.getElementById("menuMovil")
-        $menuDesktop.classList.toggle("mostar-menu-desktop")
+        // $menuDesktop.classList.toggle("mostar-menu-desktop")
         $menuMovil.classList.toggle("mostar-menu-movil")
     }
 
 
     return (
         <header className="contenedor container-fluid pt-3">
-            <div className="item-menu" onClick={mostarMenu}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" className="feather feather-menu"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg>
-            </div>
             <nav className="menu-desktop d-none d-lg-block" id="menuDesktop">
                 <div className="contenedor-menu-items-desktop">
-                    <span className="contenedor-x-menu-desktop" onClick={mostarMenu}>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
-                    </span>
                     <ul className="lista-items-menu">
                         <li>
                             <Link className="link-menu-movil" href="/">
@@ -59,6 +53,10 @@ export default function HeaderComponent(){
                     </ul>
                 </div>
             </nav>
+            <div className="item-menu d-flex d-lg-none" onClick={mostarMenu}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" className="feather feather-menu"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg>
+            </div>
+
             <nav className="menu-movil d-block d-lg-none" id="menuMovil">
                 <div className="contenedor-menu-items-movil py-4">
                     <span className="contenedor-x-menu-movil" onClick={mostarMenu}>
